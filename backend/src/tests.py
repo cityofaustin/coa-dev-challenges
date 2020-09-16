b4d747c79033d861f253100923582a1c9572fac6
Niicck <niicck@users.noreply.github.com>
2020-09-16 14:07:02 -0500

    create test for sqlite connection

--- backend/src/tests.py
+++ backend/src/tests.py
@@ -4,8 +4,12 @@ import sqlite3
 
 
 @pytest.fixture(scope='function')
-def db(tmpdir):
+def conn(tmpdir):
     file = os.path.join(tmpdir.strpath, "test.db")
     conn = sqlite3.connect(file)
     yield conn
     conn.close()
+
+
+def test_db_conn(conn):
+    assert type(conn) == sqlite3.Connection
